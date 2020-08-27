8ee9746215ba5a72038356baea41e9956c6a9658
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-27 00:18:18 -0700

    Rough login implementation in Api and Client.

--- neocaml/lib/api.ml
+++ neocaml/lib/api.ml
@@ -1,21 +1,30 @@
 open Base
+(* open Yojson *)
+
+open Types
+open Yojson_helpers
 
 let matrix_api_path   = "/_matrix/client/r0"
 let matrix_media_path = "/_matrix/media/r0"
 
+(* NOTE: Now returning String rather than Uri for easy prepend of server... *)
 let build_path ?queries ?api_path:(api=matrix_api_path) path =
   let q = queries |> Option.value_map ~f:Uri.encoded_of_query ~default:"" in
   api ^ "/" ^ Uri.pct_encode path ^ "?" ^ q
-  |> Uri.of_string
 
-(* Build a post request to server for login🥵.
- * See: https://github.com/poljar/matrix-nio/blob/master/nio/api.py
- * line 305, def login. Need to decide whether I really want to break this
- * in to Api and Client modules where so much is shared, or jsut have a client
- * module. For example, login in the nio api.py and login in async_client.py
- * https://github.com/poljar/matrix-nio/blob/master/nio/client/async_client.py
- * are almost the same. The client simply sends it and basically passes along
- * all of it's arguments. Should I follow the same pattern, or not? *)
 let login ?device_name ?device_id user cred =
-  let pth = build_path "login" in
-  ()
+  let credential =
+    match cred with
+    | Password str  -> [ ("type", `String "m.login.password")
+                       ; ("user", `String user)
+                       ; ("password", `String str)
+                       ]
+    | AuthToken str -> [ ("type", `String "m.login.token")
+                       ; ("token", `String str)
+                       ] in
+  let content =
+    credential @
+    [ ("device_id", json_of_option yo_string device_id)
+    ; ("device_name", json_of_option yo_string device_name)
+    ] |> yo_assoc in
+  (build_path "login", content)
--- neocaml/lib/client.ml
+++ neocaml/lib/client.ml
@@ -1,4 +1,8 @@
 open Base
+(* open Yojson *)
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
 
 type t = { homeserver      : string
          ; user            : string
@@ -6,9 +10,32 @@ type t = { homeserver      : string
          ; store_path      : string option
          ; access_token    : string option
          ; rooms           : (string, Room.t, String.comparator_witness) Map.t
-         ; encrypted_rooms : (string, String.comparator_witness) Set.t }
+         ; encrypted_rooms : (string, String.comparator_witness) Set.t
+         }
 
 type credential = Password of string | AuthToken of string
 
-(* TODO: Build request with Api.login and send to server. *)
-let login ?device_name client cred = ()
+let make ?device_id ?store_path ?access_token homeserver user =
+  { homeserver
+  ; user
+  ; device_id
+  ; store_path
+  ; access_token
+  ; rooms           = Map.empty (module String)
+  ; encrypted_rooms = Set.empty (module String)
+  }
+
+(* TODO: Build request with Api.login and send to server.
+ * Trying with copy of example request first... *)
+let login ?device_name client cred =
+  let pth, content =
+    Api.login ?device_name ?device_id:client.device_id client.user cred in
+  let body = content |> Yojson.Basic.to_string |> Cohttp_lwt.Body.of_string in
+  Client.post ~body (Uri.of_string (client.homeserver ^ pth))
+  >>= fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Stdio.printf "Response code: %d\n" code;
+  Stdio.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  Stdio.printf "Body of length: %d\n" (String.length body);
+  body
