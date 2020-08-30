981ba389f30159471d44f7c0327fb8b141f5a60d
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-30 13:43:11 -0700

    Helpers, couple impls, and stand-ins added.

--- neocaml/lib/api.ml
+++ neocaml/lib/api.ml
@@ -7,10 +7,18 @@ open Yojson_helpers
 let matrix_api_path   = "/_matrix/client/r0"
 let matrix_media_path = "/_matrix/media/r0"
 
-(* NOTE: Now returning String rather than Uri for easy prepend of server... *)
+let string_of_message_direction = function | Back -> "b" | Forward -> "f"
+
+let query key v = [ (key, [ v ]) ]
+
+let query_of_option key = function | None -> [] | Some v -> query key v
+
 let build_path ?queries ?api_path:(api=matrix_api_path) path =
   let q = queries |> Option.value_map ~f:Uri.encoded_of_query ~default:"" in
   api ^ "/" ^ Uri.pct_encode path ^ "?" ^ q
+
+(* Api call funcs ->
+ *  Cohttp.Code.meth * string * Yojson.Basic.t option *)
 
 let login ?device_name ?device_id user cred =
   let credential =
@@ -27,10 +35,106 @@ let login ?device_name ?device_id user cred =
     [ ("device_id", json_of_option yo_string device_id)
     ; ("device_name", json_of_option yo_string device_name)
     ] |> yo_assoc in
-  (* (build_path "login", content) *)
-  (`POST, build_path "login", content)
+  (`POST, build_path "login", Some content)
 
 let logout ?all_devices:(all=false) access_token =
   begin if all then "logout/all" else "logout" end
-  |> build_path ~queries:[("access_token", [ access_token ])]
-  |> fun pth -> (pth, yo_assoc [])
+  |> build_path ~queries:(query "access_token" access_token)
+  |> fun pth -> (`POST, pth, Some (yo_assoc []))
+
+let sync = ()
+
+let room_send = ()
+
+let room_get_event = ()
+
+let room_put_state = ()
+
+let room_get_state_event = ()
+
+let room_get_state = ()
+
+let room_redact = ()
+
+let room_kick = ()
+
+let room_ban = ()
+
+let room_unban = ()
+
+let room_invite = ()
+
+let room_create = ()
+
+let join = ()
+
+let room_leave = ()
+
+let room_forget = ()
+
+let room_messages ?stop ?dir:(dir=Back) ?lim:(lim=10) ?filter access id start =
+  let queries =
+    query_of_option "to" stop @
+    query_of_option "filter" filter @
+    [ ("access_token", [ access ])
+    ; ("from", [ start ])
+    ; ("limit", [ Int.to_string lim ])
+    ; ("dir", [ string_of_message_direction dir ])
+    ] in
+  let pth = "rooms/" ^ id ^ "/messages" in
+  (`GET, build_path ~queries pth, None)
+
+let keys_upload = ()
+
+let keys_query = ()
+
+let keys_claim = ()
+
+let to_device = ()
+
+let devices = ()
+
+let update_device = ()
+
+let delete_devices = ()
+
+let joined_members = ()
+
+let joined_rooms access_token =
+  (`GET, build_path ~queries:(query "access_token" access_token), "joined_rooms")
+
+let room_resolve_alias = ()
+
+let room_typing = ()
+
+let update_receipt_marker = ()
+
+let room_read_markers = ()
+
+let content_repository_config = ()
+
+let upload = ()
+
+let download = ()
+
+let thumbnail = ()
+
+let profile_get = ()
+
+let profile_get_displayname = ()
+
+let profile_set_displayname = ()
+
+let profile_get_avatar = ()
+
+let profile_set_avatar = ()
+
+let get_presence = ()
+
+let set_presence = ()
+
+let whoami = ()
+
+let room_context = ()
+
+let upload_filter = ()
