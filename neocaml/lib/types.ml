350f56b8a8753fbfaf42b3678b1828a695f1fae7
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-31 00:40:58 -0700

    Moved a couple types + helpers into modules.

--- neocaml/lib/types.ml
+++ neocaml/lib/types.ml
@@ -2,4 +2,27 @@ type credential = Password of string | AuthToken of string
 
 type api_triple = Cohttp.Code.meth * string * Yojson.Basic.t option
 
-type message_direction = Back | Forward
+module Presence = struct
+  type t = Offline | Online | Unavailable
+
+  let to_string = function
+    | Offline     -> "offline"
+    | Online      -> "online"
+    | Unavailable -> "unavailable"
+
+  (* Not sure if needed yet. *)
+  let of_string = function
+    | "offline"     -> Offline
+    | "online"      -> Online
+    | _             -> Unavailable
+end
+
+module Message_direction = struct
+  type t = Back | Forward
+
+  let to_string = function | Back -> "b" | Forward -> "f"
+end
+
+module Room_events = struct
+  type t
+end
