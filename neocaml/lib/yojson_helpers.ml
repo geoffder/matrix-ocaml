842bf9036d567fcbc1d3ca5a181e78578c52e651
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-09-01 23:29:31 -0700

    Constrain yojson helpers return types.

--- neocaml/lib/yojson_helpers.ml
+++ neocaml/lib/yojson_helpers.ml
@@ -1,10 +1,11 @@
 open Base
 
-let yo_assoc l  = `Assoc l
-let yo_string s = `String s
-let yo_float f  = `Float f
-let yo_int i    = `Int i
-let yo_bool b   = `Bool b
+let yo_assoc l  : Yojson.Basic.t = `Assoc l
+let yo_string s : Yojson.Basic.t = `String s
+let yo_float f  : Yojson.Basic.t = `Float f
+let yo_int i    : Yojson.Basic.t = `Int i
+let yo_bool b   : Yojson.Basic.t = `Bool b
+let yo_list l   : Yojson.Basic.t = `List l
 
 let json_of_option con opt : Yojson.Basic.t =
   Option.value_map ~f:con ~default:`Null opt
