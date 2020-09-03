635ff335de946c965c8191a203419bc3dce3cef6
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-09-03 01:16:48 -0700

    Switch from Basic to Safe

--- neocaml/lib/yojson_helpers.ml
+++ neocaml/lib/yojson_helpers.ml
@@ -1,11 +1,11 @@
 open Base
 
-let yo_assoc l  : Yojson.Basic.t = `Assoc l
-let yo_string s : Yojson.Basic.t = `String s
-let yo_float f  : Yojson.Basic.t = `Float f
-let yo_int i    : Yojson.Basic.t = `Int i
-let yo_bool b   : Yojson.Basic.t = `Bool b
-let yo_list l   : Yojson.Basic.t = `List l
+let yo_assoc l  : Yojson.Safe.t = `Assoc l
+let yo_string s : Yojson.Safe.t = `String s
+let yo_float f  : Yojson.Safe.t = `Float f
+let yo_int i    : Yojson.Safe.t = `Int i
+let yo_bool b   : Yojson.Safe.t = `Bool b
+let yo_list l   : Yojson.Safe.t = `List l
 
-let json_of_option con opt : Yojson.Basic.t =
+let json_of_option con opt : Yojson.Safe.t =
   Option.value_map ~f:con ~default:`Null opt
