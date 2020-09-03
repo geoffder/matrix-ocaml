142f920850223361c1db89f2d84df2c0a899778d
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-09-03 01:15:02 -0700

    Add Room.Config module (used in Api.create_room)

--- neocaml/lib/room.ml
+++ neocaml/lib/room.ml
@@ -1,3 +1,40 @@
-(* open Base *)
+open Base
+open Yojson_helpers
 
 type t
+
+module Config = struct
+  type vis = Private | Public
+
+  let vis_to_string = function | Private -> "private" | Public -> "public"
+
+  let vis_of_string = function | "private" -> Private | _ -> Public
+
+  let vis_to_yojson = Fn.compose yo_string vis_to_string
+
+  type preset = PrivateChat | TrustedPrivateChat | PublicChat
+
+  let preset_to_string = function
+    | PrivateChat        -> "private_chat"
+    | TrustedPrivateChat -> "trusted_private_chat"
+    | PublicChat         -> "public_chat"
+
+  let preset_to_yojson = Fn.compose yo_string preset_to_string
+
+  let preset_option_to_json p =
+    p |> Option.map ~f:preset_to_string |> json_of_option yo_string
+
+  let federate_to_json f = yo_assoc [ ("m.federate", yo_bool f) ]
+
+  type t =
+    { visibility   : vis
+    ; alias        : string option
+    ; name         : string option [@key "room_alias_name"]
+    ; topic        : string option
+    ; room_version : string option
+    ; federate     : bool [@key "creation_content"] [@to_yojson federate_to_json]
+    ; preset       : preset option
+    ; is_direct    : bool
+    } [@@deriving to_yojson]
+
+end
