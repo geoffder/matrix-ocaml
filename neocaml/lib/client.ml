82bc44c3168a72ccde460c6e31cf5f63cf9aeea1
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-09-03 23:26:13 -0700

    Wire up send with repeat. Now using Lwt_result.

--- neocaml/lib/client.ml
+++ neocaml/lib/client.ml
@@ -6,6 +6,7 @@ open Cohttp_lwt_unix
 
 (* open Types *)
 (* open Yojson_helpers *)
+open Neo_infix
 
 type t = { homeserver      : string
          ; user            : string
@@ -30,28 +31,23 @@ let make ?device_id ?store_path ?access_token homeserver user =
 
 let complete_uri client pth = client.homeserver ^ pth |> Uri.of_string
 
-let response_code = Response.status |> Fn.compose Code.code_of_status
+let response_code = Response.status >> Code.code_of_status
 
 let body_of_json j = j |> Yojson.Safe.to_string |> Cohttp_lwt.Body.of_string
 
 let json_of_body b = b |> Cohttp_lwt.Body.to_string >|= Yojson.Safe.from_string
 
+(* NOTE: Both of these should be unnecessary now... *)
+(* Since Cohttp_lwt.Body lives in Lwt, double flat mapping is required. *)
+let lwt_body_result_map ~f r_lwt = Lwt_result.map (fun b_lwt -> b_lwt >|= f) r_lwt
+let ( >>|= ) a f = lwt_body_result_map ~f a
+
 (* Continue execution of given function if logged in. *)
 let logged_in client f =
   match client.access_token with
-  | None ->
-    Stdio.print_endline "Not logged in.";
-    Lwt.return client
+  | None -> Lwt.return_error "Not logged in."
   | Some token -> f token
 
-(* ctx would come from client if actually needed? See where it is used in nio *)
-(* Create call closure so that it can be recursively called until response is
- * received (e.g. not 429 code), as with _send in nio async_client.
- *
- * Need to figure out my error response handing architecture (general in here
- * and specific in the calling functions). Or have a handler that only gets called
- * after send has returned, but it has the general cases closed in, and accepts
- * specific ones as a parameter *)
 let with_timeout ?timeout ~call uri =
   let times_up =
     timeout
@@ -60,13 +56,13 @@ let with_timeout ?timeout ~call uri =
       ~f:(fun i -> i / 1000 |> Int.to_float)
     |> Lwt_unix.sleep
     >>= fun () -> Lwt.return_error `TimedOut in
-  let called = call uri >>= fun response -> Lwt.return_ok response in
+  let called = call uri >>= Lwt.return_ok in
   Lwt.pick [ times_up; called ]
 
-(* TODO: Add in data provider. Needs re-structuring of the call closure etc. *)
-let repeat ?timeout ?(max_429s=100) ?(max_outs=100) ~call uri =
+let repeat ?timeout ?(max_429s=100) ?(max_outs=100) ~call ~get_data meth uri =
   let rec aux n_429s n_outs =
-    with_timeout ?timeout ~call uri
+    let loaded = call ?body:(get_data ()) meth in
+    with_timeout ?timeout ~call:loaded uri
     >>= function
     | Ok (resp, _body as response) ->
       if phys_equal resp.Response.status `Too_many_requests
@@ -81,17 +77,13 @@ let repeat ?timeout ?(max_429s=100) ?(max_outs=100) ~call uri =
       else Lwt.return_error "Hit maximum timeouts." in
   aux 0 0
 
-(* TODO: Need to change call closure creation to suit repeat. Also, need to
- * propagate error through if I am going to do it this way e.g. with repeating
- * as they do in nio. *)
+(* ctx would come from client if actually needed? See where it is used in nio *)
 let send
     ?ctx
     ?content_type
     ?content_len
-    (* ?timeout *)
-    (* ?data_provider *) (* NOTE: Need to not close body in to call before
-                          * passing to repeat in order to be able to refresh
-                          * the data used to create the body. *)
+    ?timeout
+    ?data_provider
     client
     (meth, pth, content)
   =
@@ -104,14 +96,20 @@ let send
     |> Header.of_list in
   let uri = complete_uri client pth in
   let body = Option.map ~f:body_of_json content in
-  let call = Client.call ?ctx ~headers ?body meth in
-  call uri >>= fun (_resp, body) -> body |> json_of_body
+  let get_data =
+    match data_provider with
+    | Some dp -> dp
+    | None    -> fun () -> body in
+  let call = Client.call ?ctx ?chunked:None ~headers in
+  repeat ?timeout ~call ~get_data meth uri
+  >=> fun (_resp, body) ->
+    Cohttp_lwt.Body.to_string body
+    >>= ( Yojson.Safe.from_string >> Lwt.return_ok )
 
 let login ?device_name client cred =
   Api.login ?device_name ?device_id:client.device_id client.user cred
   |> send client
-  >|= fun j ->
-  (* TODO: handling / call backs ? *)
+  >>| fun j ->
   let open Yojson.Safe.Util in
   { client with
     user_id      = j |> member "user_id" |> to_string_option
@@ -121,41 +119,29 @@ let login ?device_name client cred =
 
 let logout ?all_devices:(all_devices=false) client =
   match client.access_token with
-  | None -> Lwt.return client
+  | None -> Lwt.return_error "Already logged out."
   | Some token ->
     Api.logout ~all_devices token
     |> send client
-    >|= fun _ ->
-    (* nio _handle_logout checks for ErrorResponse, doesn't clear if so... *)
-    (* TODO: handling / call backs *)
-    { client with access_token = None }
+    >>| fun _ -> { client with access_token = None }
 
 let joined_rooms client =
   logged_in client begin fun token ->
     Api.joined_rooms token
     |> send client
-    >|= fun j ->
-    (* TODO: handling / call backs *)
-    Yojson.Safe.to_string j |> Stdio.print_endline;
-    client
+    >>| Yojson.Safe.to_string
   end
 
 let room_messages ?stop ?dir ?lim:(lim=10) ?filter client id start =
   logged_in client begin fun token ->
     Api.room_messages ?stop ?dir ~lim ?filter token id start
     |> send client
-    >|= fun j ->
-    (* TODO: handling / call backs *)
-    Yojson.Safe.to_string j |> Stdio.print_endline;
-    client
+    >>| Yojson.Safe.to_string
   end
 
 let sync ?since ?timeout ?filter ?full_state:(full=false) ?set_presence client =
   logged_in client begin fun token ->
     Api.sync ?since ?timeout ?filter ~full_state:full ?set_presence token
     |> send client
-    >|= fun j ->
-    (* TODO: handling / call backs *)
-    Yojson.Safe.to_string j |> Stdio.print_endline;
-    client
+    >>| Yojson.Safe.to_string
   end
