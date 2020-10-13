a46e18f59e2682d9b0156d086efc83d55e73d7db
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-12 23:55:50 -0700

    Change Lwt_result bind infix to match Async infix.

--- neocaml/lib/neo_infix.ml
+++ neocaml/lib/neo_infix.ml
@@ -3,7 +3,7 @@ let ( >>| ) a f = Lwt_result.map f a
 
 (* Lwt_result.bind takes value as first arg, and func as second. So this cleans
  * things up a bit for me. *)
-let ( >=> ) = Lwt_result.bind
+let ( >>=? ) = Lwt_result.bind
 
 (* Infix of Fn.compose *)
 let ( >> ) g f x = f (g x)
