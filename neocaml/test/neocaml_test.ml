d8a26a371124d85b868bf0ab3c72badf78456b73
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-11-05 23:44:16 -0800

    Correct for new client arg ordering

--- neocaml/test/neocaml_test.ml
+++ neocaml/test/neocaml_test.ml
@@ -2,7 +2,7 @@ open Core
 open Neocaml
 open Neocaml.Neo_infix
 
-let client = Client.make "https://shakeandwake.xyz" "@test_user:matrix.shakeandwake.xyz"
+let client = Client.create "https://shakeandwake.xyz" "@test_user:matrix.shakeandwake.xyz"
 
 (* For simple relative pathing from the running directory, which for this module
  * is neocaml/bin/default/test. *)
@@ -18,12 +18,12 @@ let logged =
              |> Yojson.Safe.from_file
              |> Yojson.Safe.Util.member "password"
              |> Yojson.Safe.Util.to_string in
-  Types.Credential.Password pass |> Client.login client
+  Client.login (Types.Credential.Password pass) client
 
 let room_id = "!IYzufTUjKQbVlmtONr:matrix.shakeandwake.xyz"
 let prev_batch = ""
 
-let messages = logged >>=? fun c -> Client.room_messages c room_id prev_batch
+let messages = logged >>=? Client.room_messages room_id prev_batch
 
 let%test "room messages" = Lwt_main.run messages |> Result.is_ok
 
@@ -43,17 +43,17 @@ let send_poggers () =
       ~formatted_body:formatted
       ":pogchamp: wew :pogchamp: lad :pogchamp:"
     |> Message.text in
-  logged >>=? fun c -> Client.room_send c room_id (Content.Message content)
+  logged >>=? Client.room_send room_id (Content.Message content)
 
 let send_emote () =
   let open Events.Room in
   let content = Message.Emote.create "smiles creepily." |> Message.emote in
-  logged >>=? fun c -> Client.room_send c room_id (Content.Message content)
+  logged >>=? Client.room_send room_id (Content.Message content)
 
 let send_notice () =
   let open Events.Room in
   let content = Message.Notice.create "You're on notice!" |> Message.notice in
-  logged >>=? fun c -> Client.room_send c room_id (Content.Message content)
+  logged >>=? Client.room_send room_id (Content.Message content)
 
 let img_pth = "/home/" ^ Unix.getlogin () ^ "/Pictures/emojis/bigben.png"
 let send_image () =
