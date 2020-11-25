d6f7e4fef4155c46a1052edb736a0f2800f8a67c
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-11-24 21:44:42 -0800

    WIP json payload generation functions
                                # Please enter the commit message for your changes. Lines starting
                                                                                          # with '#' will be ignored, and an empty message aborts the commit.
                                                                                                                                                        #
                                                                                                                                                          # On branch master
                                                                                                                                                                      # Your branch is ahead of 'origin/master' by 1 commit.
                                                                                                                                                                                                                       #   (use "git push" to publish your local commits)
      # Changes to be committed:
                     #	modified:   neocaml/lib/neolm.ml
                                               #
                                                 # Changes not staged for commit:
                                                                            #	modified:   neocaml/lib/event.ml
                                                                                                      #	modified:   neocaml/lib/toDevice.ml
                                                                                                                                #

--- neocaml/lib/neolm.ml
+++ neocaml/lib/neolm.ml
@@ -20,6 +20,7 @@ let message_index_store_size           = 100000
  * (or break down and make a local type for it here...) *)
 type t = { user_id                 : string
          ; device_id               : string
+         ; account                 : Account.t
          ; uploaded_key_count      : int
          ; users_for_key_query     : (string, String.comparator_witness) Set.t
          ; device_store            : DeviceStore.t
@@ -30,3 +31,42 @@ type t = { user_id                 : string
          ; outgoing_key_requests   : OutgoingKeyRequest.t StringMap.t
          ; received_key_requests   : IncomingKeyRequest.t StringMap.t
          }
+
+module Payloads = struct
+  let olm_algorithm    = `String olm_algorithm
+  let megolm_algorithm = `String megolm_algorithm
+  let algorithms       = `List (List.map ~f:yo_string algorithms)
+
+  let sign_json account j = Api.canonical_json j |> Account.sign account
+
+  let device_keys t =
+    Account.identity_keys t.account >>= fun ids ->
+    let keys = [ ("algorithms", algorithms)
+               ; ("device_id", `String t.device_id)
+               ; ("user_id", `String t.user_id)
+               ; ("keys", [ ("curve25519:" ^ t.device_id, `String ids.curve25519)
+                          ; ("ed25519:" ^ t.device_id, `String ids.ed25519)
+                          ] |> yo_assoc)
+               ] |> yo_assoc
+    in
+    sign_json t.account keys >>| fun signature ->
+    `Assoc [
+      ("signatures", `Assoc [
+          ("ed25519:" ^ t.device_id, `String signature) ]) ]
+    |> U.combine keys
+
+  let one_time_keys t =
+    let f acc ~key ~data =
+      sign_json t.account (`Assoc [ ("key", `String data) ]) >>| fun signature ->
+      let signatures = [ (t.user_id, [ ("ed25519:" ^ t.device_id, `String signature)
+                                     ] |> yo_assoc)
+                       ] |> yo_assoc
+      in
+      ("signed_curve25519:" ^ key, [ ("key", `String data)
+                                   ; ("signatures", signatures)
+                                   ] |> yo_assoc)
+      :: acc
+    in
+    Account.one_time_keys t.account >>= fun { curve25519 = keys } ->
+    Neolm_utils.map_fold_result ~init:[] ~f keys >>| yo_assoc
+end
